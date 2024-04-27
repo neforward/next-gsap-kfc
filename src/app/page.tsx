96e71bdee44d4ad85bf8c8beb4843893ab7d5aa0
neforward <141821349+neforward@users.noreply.github.com>
2024-04-27 20:00:42 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,60 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header-content">
+            <div className="header-logo">
+              <img
+                src="https://devkfcwebsites.fimble.io/admin/files/4672.svg"
+                alt=""
+              />
+            </div>
+            <div className="header-nav">
+              <span>Menu</span>
+              <span>About</span>
+              <span>Restaurants</span>
+              <span>Where To Buy</span>
+              <span>Locations</span>
+              <span></span>
+            </div>
+            <div className="header-cart">
+              <img
+                src="https://yuzeyzer.github.io/ReactJS__Doritos/static/media/cart.dc0d4b90.svg"
+                alt=""
+              />
+              <div className="counter">
+                <p>2</p>
+              </div>
+            </div>
+          </div>
         </div>
+      </header>
+      <div className="hero">
+        <div className="container">
+          <h2>a fiery burst of flavor that sparks pure satisfaction.</h2>
+          <h5>KFC Chicken Wings</h5>
+          <p>
+            Chicken, Flour (Wheat Flour, Niacin, Reduced Iron, Thiamine
+            Mononitrate, Riboflavin, Folic Acid), Salt, Spices (Including Black
+            Pepper, White Pepper, Paprika), Monosodium Glutamate, Garlic Powder,
+            Onion Powder, Buttermilk Powder (Cultured Buttermilk, Whey), Corn
+            Starch, Natural Flavorings, Sodium Phosphates, Breading Set in
+            Vegetable Oil (Soybean Oil, Hydrogenated Soybean Oil with TBHQ and
+            Citric Acid added to preserve freshness, Dimethylpolysiloxane, an
+            Anti-foaming Agent added).
+          </p>
+          <strong>CONTAINS: WHEAT INGREDIENTS</strong>
+          <h4>$34.99</h4>
+          <button>Add to cart</button>
+        </div>
+        {/* <img src="https://yuzeyzer.github.io/ReactJS__Doritos/static/media/suyaPepper.07c5bdc5.png" alt="" /> */}
+        {/* <img src="https://cdn.culvers.com/menu/images/item/sauces-dressings/wi-cheddar-cheese.png?q=90&w=800&format=auto" alt="" /> */}
+        {/* <img src="https://pngimg.com/d/kfc_food_PNG50.png" alt="" /> */}
+        {/* <img src="https://pngimg.com/uploads/fried_chicken/fried_chicken_PNG14104.png" alt="" />  */}
+        {/* <img src="https://pngimg.com/d/fried_chicken_PNG97915.png" alt="" /> */}
+        {/* <img src="https://images.ctfassets.net/9tka4b3550oc/1FQSRLVXt2Q1lvXXkOyW6U/f306561ef7bfc5ab7c84a739a46d3629/Food_09.png?q=75&w=1280" alt="" /> */}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
